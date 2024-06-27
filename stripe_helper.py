b187ecf9456432f18b0e95f452c67b8cd495b452
armaanthakker <armaanthakker@gmail.com>
2024-06-27 00:58:39 -0700

    3

--- stripe_helper.py
+++ stripe_helper.py
@@ -5,12 +5,12 @@ import streamlit as st
 
 
 # Load environment variables
-openai_key = st.secrets["OPENAI_API_KEY"]
-wcd_api_key = st.secrets["WCD_API_KEY"]
-wcd_url = st.secrets["WCD_URL"]
-stripe_secret_key = st.secrets["STRIPE_SECRET_KEY"]
-success_url = st.secrets["SUCCESS_URL"]
-cancel_url = st.secrets["CANCEL_URL"]
+openai_key = st.secrets["default"]["OPENAI_API_KEY"]
+wcd_api_key = st.secrets["default"]["WCD_API_KEY"]
+wcd_url = st.secrets["default"]["WCD_URL"]
+stripe_secret_key = st.secrets["default"]["STRIPE_SECRET_KEY"]
+success_url = st.secrets["default"]["SUCCESS_URL"]
+cancel_url = st.secrets["default"]["CANCEL_URL"]
 
 # Set Stripe API key
 stripe.api_key = stripe_secret_key
